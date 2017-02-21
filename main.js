68e203e978aa3ad718ae075d68e7b2d2277e43e6
Haasith Sanka <haasithsanka@Haasiths-MacBook-Pro.local>
2017-02-20 18:19:10 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+ $(document).ready(function(){
+
+ $('.slider').slider({full_width: false});
+ $('.slider').slider('start');
+  $('ul.tabs').tabs();
+   $('.carousel').carousel();
+   $('ul.tabs').tabs('select_tab', 'one');
+
+  });
+
+ // var options = [
+ //    {selector: '.class', offset: 200, callback: customCallbackFunc } },
+ //    {selector: '.other-class', offset: 200, callback: function() {
+ //      customCallbackFunc();
+ //    } },
+ //  ];
+ //  Materialize.scrollFire(options);
+        
+        
+
+
